ebfbe0b03fd18f131eec6a98a8c72c2cfe3f4c9e
Jim Donelson <jim.donelson@sonavation.com>
2015-11-13 16:28:02 -0500

    Add life.

---
+++ workspace_maps/UCSDUnfoldingMaps/src/jimscode/LifeExpectancy.java
@@ -0,0 +1,28 @@
+package jimscode;
+
+
+import processing.core.PApplet;
+import de.fhpotsdam.unfolding.UnfoldingMap;
+import de.fhpotsdam.unfolding.utils.MapUtils;
+import de.fhpotsdam.unfolding.providers.*;
+import de.fhpotsdam.unfolding.providers.Google.*;
+
+import java.util.List;
+import de.fhpotsdam.unfolding.data.Feature;
+import de.fhpotsdam.unfolding.data.GeoJSONReader;
+
+import java.util.HashMap;
+
+public class LifeExpectancy extends PApplet {
+	UnfoldingMap map;
+	public void setup(){
+		size(800,600,OPENGL);
+		map = new UnfoldingMap(this, 50, 50, 700, 500, new Google.GoogleMapProvider());
+		MapUtils.createDefaultEventDispatcher(this, map);
+		
+	}
+	public void draw(){
+		map.draw();
+	}
+
+}
