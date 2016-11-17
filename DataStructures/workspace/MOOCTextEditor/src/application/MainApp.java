7565fa7d6dd7086f7c58f41a2e9dd00917cf0427
Jim Donelson <jcdonelson@192.168.11.117>
2016-11-16 20:45:55 -0500

    Just A Test

--- DataStructures/workspace/MOOCTextEditor/src/application/MainApp.java
+++ DataStructures/workspace/MOOCTextEditor/src/application/MainApp.java
@@ -1,5 +1,5 @@
 package application;
-	
+
 import java.io.IOException;
 import java.util.List;
 
@@ -26,36 +26,36 @@ import javafx.scene.text.Text;
 public class MainApp extends Application {
 	private Stage primaryStage;
 	private BorderPane rootLayout;
-	
-	
-	// called at start of application
+
+
+	// called at start of application.
 	@Override
 	public void start(Stage primaryStage) {
-		
+
 		this.primaryStage = primaryStage;
-		
+
 		this.primaryStage.setTitle("TextProApp");
-		
+
 		try {
 			// Load root layout from fxml
 			FXMLLoader loader = new FXMLLoader(MainApp.class.getResource("view/RootLayout.fxml"));
 			rootLayout = (BorderPane) loader.load();
             Scene scene = new Scene(rootLayout);
             primaryStage.setScene(scene);
-            
+
             // min height and width calculated from components in TextAppLayout
             primaryStage.setMinHeight(430);
             primaryStage.setMinWidth(334);
             primaryStage.show();
-            
-          
+
+
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
-		
+
 		showTextProApp();
 	}
-	
+
 	/**
      * Shows the main TextApplication scene
      */
@@ -63,27 +63,27 @@ public class MainApp extends Application {
         try {
             // Load the fxml file and set into the center of the main layout
             FXMLLoader loader = new FXMLLoader(getClass().getResource("view/TextAppLayout.fxml"));
-            
+
             HBox textProPage = (HBox) loader.load();
             rootLayout.setCenter(textProPage);
-            
+
             // Connect controller and main app
             TextProController controller = loader.getController();
             controller.setMainApp(this);
-            
+
         } catch (IOException e) {
             // Exception gets thrown if the fxml file could not be loaded
             e.printStackTrace();
         }
     }
-    
-    
-    
+
+
+
     // SHOW NEW STAGE METHODS
-    
+
     /**
      * Shows dialog for user input error
-     * 
+     *
      * @param inErr - message to dispaly
      */
     public void showInputErrorDialog(String inErr) {
@@ -94,12 +94,12 @@ public class MainApp extends Application {
 
 		alert.showAndWait();
     }
-    
+
     /**
      * Displays dialog that allows user to select local text file to display in TextArea
-     * 
+     *
      * @param ta - reference to TextArea to display loaded text file
-     * 
+     *
      */
     public void showLoadFileDialog(AutoSpellingTextArea ta) {
     	try {
@@ -116,24 +116,24 @@ public class MainApp extends Application {
 			// Set reference to stage in controller
 			LoadFileDialogController controller = loader.getController();
 			controller.setDialogStage(dialogStage);
-			
+
 			// give controller reference to text area to load file into
 			controller.setTextArea(ta);
 
 			// Show the dialog and wait until the user closes it
 		    dialogStage.showAndWait();
-		    
-		    
-		
+
+
+
 
     	} catch (IOException e) {
     		// Exception gets thrown if the fxml file could not be loaded
     		e.printStackTrace();
     	}
-    	
+
     }
-    
- 
+
+
     public void showEditDistanceDialog(String selectedText) {
     	try {
     		// Load the fxml file and create a new stage for the popup
@@ -151,23 +151,23 @@ public class MainApp extends Application {
 			controller.setDialogStage(dialogStage);
 			controller.setMainApp(this);
 			controller.setField(selectedText);
-			
-			
+
+
 			// give controller reference to scene (cursor)
 
 			// Show the dialog and wait until the user closes it
 		    dialogStage.showAndWait();
-		    
-		    
-		
+
+
+
 
     	} catch (IOException e) {
     		// Exception gets thrown if the fxml file could not be loaded
     		e.printStackTrace();
     	}
-    	
+
     }
-    
+
     public void showEDResult(List<String> path) {
         // intialize alert/dialog to display edit distance result
     	Alert alert = new Alert(AlertType.INFORMATION);
@@ -175,49 +175,49 @@ public class MainApp extends Application {
     	alert.setHeaderText("Word Path : ");
     	alert.initModality(Modality.NONE);
     	alert.setResizable(true);
-    	
+
     	// create layout for content
     	VBox box = new VBox();
     	HBox midBox = new HBox();
     	box.setPadding(new Insets(35,0,35,0));
     	box.setSpacing(35);
     	midBox.setSpacing(15);
-    	
+
     	Label pathLabel = new Label();
     	Label numStepsLabel = new Label("Number of steps : ");
     	Label numSteps = new Label();
     	Font font = new Font(14);
     	pathLabel.setFont(font);
-    	numStepsLabel.setFont(font);    	
+    	numStepsLabel.setFont(font);
     	numSteps.setFont(Font.font(font.getFamily(), FontWeight.BOLD, 14));
-    	
+
     	midBox.getChildren().add(numStepsLabel);
     	midBox.getChildren().add(numSteps);
     	midBox.setAlignment(Pos.CENTER);
-    	
+
     	box.getChildren().add(pathLabel);
     	box.getChildren().add(midBox);
     	box.setAlignment(Pos.CENTER);
     	alert.getDialogPane().setPrefWidth(300);
-    	
+
     	// check for path
     	if(path != null) {
     		numSteps.setText(Integer.toString(path.size()-1));
 	    	pathLabel.setText(String.join(" -> ", path));
-	    	
+
 	    	Text text = new Text(pathLabel.getText());
 	    	text.setFont(font);
 	    	if(text.getLayoutBounds().getWidth() > 200) {
 		    	alert.getDialogPane().setPrefWidth(text.getLayoutBounds().getWidth()+100);
 	    	}
-	    	
+
     	}
     	// no path found
     	else {
     		pathLabel.setText("No Path Found.");
     		numSteps.setText("N/A");
     	}
-    	
+
     	// set content and styling
     	alert.getDialogPane().setContent(box);
     	alert.getDialogPane().getStylesheets().add(
@@ -225,8 +225,8 @@ public class MainApp extends Application {
     	alert.getDialogPane().getStyleClass().add("myDialog");
     	alert.showAndWait();
     }
-    
-    
+
+
     public void showMarkovDialog(textgen.MarkovTextGenerator mtg) {
     	try {
     		// Load the fxml file and create a new stage for the popup
@@ -248,18 +248,18 @@ public class MainApp extends Application {
 
 			// Show the dialog and wait until the user closes it
 		    dialogStage.showAndWait();
-		    
-		    
-		
+
+
+
 
     	} catch (IOException e) {
     		// Exception gets thrown if the fxml file could not be loaded
     		e.printStackTrace();
     	}
-    	
-    	
+
+
     }
-    
+
     public void showLoadStage(Stage loadStage, String text) {
     	loadStage.initModality(Modality.APPLICATION_MODAL);
     	loadStage.initOwner(primaryStage);
@@ -274,15 +274,15 @@ public class MainApp extends Application {
         loadStage.setScene(loadScene);
         loadStage.show();
     }
-    
-    
+
+
     // MAIN
 	public static void main(String[] args) {
 		launch(args);
 	}
-	
+
 	public Stage getStage() {
 		return this.primaryStage;
 	}
-	
+
 }
